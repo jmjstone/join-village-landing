1baacc7e7eeb01feda22590f29d277b162ec3170
jmjstone <jayden.m.jstone@gmail.com>
2025-09-17 23:30:37 -0400

    spacing changes, simpler hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col justify-items-center justify-around m-auto">
+    <div className="pt-5 flex flex-col justify-items-center justify-around m-auto">
       <Image
         className="flex flex-1 hover:scale-102 transition duration-400 m-auto animate-fade-in-up animation-delay-200"
         src="/VillageMockupHomeCropped.png"
@@ -12,16 +12,13 @@ const Hero = () => {
         width={300}
       />
       <div className="flex flex-1 m-auto text-center flex-col animate-fade-in-up animation-delay-400">
-        <div className="flex flex-col gap-5">
-          <div className="text-5xl text-black max-w-200 font-bold animate-fade-in-up animation-delay-600">
-            Find Community. Build a brand. Get Paid.
-          </div>
-          <div className="text-5xl text-black max-w-200 font-bold animate-fade-in-up animation-delay-800">
-            All in one place.
+        <div className="flex flex-col gap-10">
+          <div className="pt-5 text-5xl text-black max-w-300 font-bold animate-fade-in-up animation-delay-600">
+            Find community. Build your brand. Get paid.
           </div>
         </div>
         <Button
-          className="animate-fade-in-up animation-delay-1000"
+          className="mt-15 animate-fade-in-up animation-delay-1000"
           href="https://tally.so/r/w22QYp"
         >
           Join the Mission
